dee221a558774c2ba10c0e8e99bf7e962f61c140
ledez gregory <ledez.greg@gmail.com>
2022-12-08 13:07:37 +0100

    modified size of pictures

--- src/components/ProjetCard.jsx
+++ src/components/ProjetCard.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import './projetCard.scss';
+import sparkle from '../assets/img/sparkle.png';
 
 function ProjetCard({ projet }) {
   return (
     <div className="projetCard">
-      <img className="projetCard__sparkle" src="src/assets/img/sparkler (2).png" alt="" />
-      {/* <span className="projetCard__id">{projet.id}</span> */}
+      <img className="projetCard__sparkle" src={sparkle} alt="icon" />
       <a target="_blank" href={projet.github} rel="noreferrer">
         <h4>{projet.title}</h4>
-        <img className="projetCard__img" src={projet.photo} alt="github link" />
+        <img className="projetCard__img" src={projet.photo} alt="projet" />
       </a>
       <p>{projet.txt}</p>
       <div className="stacks">

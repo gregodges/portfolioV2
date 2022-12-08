d3a8f264aeca13ba571989f67a9a39f76aa5a80a
ledez gregory <ledez.greg@gmail.com>
2022-12-08 09:07:53 +0100

    samll ajustements

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './header.scss';
 import Alternance from './pages/Alternance';
-import Home from './pages/home';
+import Home from './pages/Home';
 
 function App() {
   return (
